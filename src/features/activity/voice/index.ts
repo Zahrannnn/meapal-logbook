8da0cb3b67886227067b699e9efe3a685f47173b
Zahrannnn <102045122+Zahrannnn@users.noreply.github.com>
2026-09-05 18:11:15 +0300

    feat(voice): add index.ts

---
+++ src/features/activity/voice/index.ts
@@ -0,0 +1,4 @@
+export { VoiceActivityModal } from './components/VoiceActivityModal';
+export { useVoiceRecorder } from './hooks/useVoiceRecorder';
+export { voiceService } from './services/voice.service';
+export { adaptParsedVoiceActivityToDraftPatch } from './mappers/voiceParse.adapter';
