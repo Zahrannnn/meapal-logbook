75763bf5a34bc731d80fa0c1d0f3ea1cb01cf407
Zahrannnn <102045122+Zahrannnn@users.noreply.github.com>
2026-09-05 18:11:00 +0300

    feat(auth): add index.ts

---
+++ src/features/auth/index.ts
@@ -0,0 +1,4 @@
+export { LoginPage } from './components/LoginPage';
+export { ForgotPasswordModal } from './components/ForgotPasswordModal';
+export { useForgotPasswordFlow } from './hooks';
+export { authService, convertBackendUserToFrontend } from './services/auth.service';
